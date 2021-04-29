435d54179fdf5118e19ca3c78ce41951092a5257
emikoo <emikoo@users.noreply.github.com>
2021-04-29 16:40:47 +0600

    layout for projects

--- app/src/main/java/com/example/notesapp/ui/projects/ProjectActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/projects/ProjectActivity.kt
@@ -1,11 +1,12 @@
-package com.example.notesapp
+package com.example.notesapp.ui.projects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notesapp.R
 
-class MainActivity : AppCompatActivity() {
+class ProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_project)
     }
 }
